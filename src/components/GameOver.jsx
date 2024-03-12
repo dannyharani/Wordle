a9e1b503cbab87e27c80eed12d6bfc0228b3d708
Danny Harani <danniharany@gmail.com>
2024-03-12 12:32:04 -0400

    Updated to show pluar of tries

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -4,7 +4,7 @@ function GameOver({guessCount, win, correctWord}) {
   return (
     <div>
         <h1>Game Over</h1>
-        {win ? <h3>It took you {guessCount} try to guess the correct word: </h3> : <h3>The correct word was:</h3>}
+        {win ? (guessCount>=1 ? <h3>It took you {guessCount} tries to guess the correct word: </h3> : <h3>It took you {guessCount} try to guess the correct word: </h3>) : <h3>The correct word was:</h3>}
         <h2>{correctWord}</h2>
     </div>
   )
